9e491b51cee00144c348aba0f8c2979bd6081441
Xuanfq <2624208682@qq.com>
2024-06-29 12:45:33 +0800

    update api test script to support api auth

--- api/test/test-api.sh
+++ api/test/test-api.sh
@@ -3,7 +3,8 @@ set -x
 
 API_PORT=5000
 API_NAME=api
-
+API_AUTH_USERNAME=admin
+API_AUTH_PASSWORD=admin
 
 createfolderpath=folder1
 
@@ -21,27 +22,30 @@ deletefilepath=$uploadpath/$uploadfile
 
 deletefolderpath=$uploadpath
 
+AUTH_RESULT=$(curl -X POST http://localhost:$API_PORT/$API_NAME/login -d username=$API_AUTH_USERNAME -d password=$API_AUTH_PASSWORD)
+AUTH_TOKEN=$(echo $AUTH_RESULT | grep -o '"data":"[^"]*"' | cut -d':' -f2- | tr -d '"')
+echo $AUTH_TOKEN
 
 echo create folder
-curl http://127.0.0.1:$API_PORT/$API_NAME/add/folder -X POST -d "path=$createfolderpath"
+curl http://127.0.0.1:$API_PORT/$API_NAME/add/folder -X POST -d "path=$createfolderpath" -d "token=$AUTH_TOKEN"
 
 echo upload file
-curl http://127.0.0.1:$API_PORT/$API_NAME/upload/file -X POST -F "file=@$uploadfile" -F "path=$uploadpath"
+curl http://127.0.0.1:$API_PORT/$API_NAME/upload/file -X POST -F "file=@$uploadfile" -F "path=$uploadpath" -F "token=$AUTH_TOKEN"
 
 echo download file
-curl http://127.0.0.1:$API_PORT/$API_NAME/download/file?path=$downloadpath
+curl http://127.0.0.1:$API_PORT/$API_NAME/download/file?path=$downloadpath\&token=$AUTH_TOKEN
 
 echo search
-curl http://127.0.0.1:$API_PORT/$API_NAME/search/file?path=$searchpath\&key=$searchkey
+curl http://127.0.0.1:$API_PORT/$API_NAME/search/file?path=$searchpath\&key=$searchkey\&token=$AUTH_TOKEN
 
 echo list path contents
-curl http://127.0.0.1:$API_PORT/$API_NAME/get/list?path=$listcontentpath
+curl http://127.0.0.1:$API_PORT/$API_NAME/get/list?path=$listcontentpath\&token=$AUTH_TOKEN
 
 echo delete file
-curl http://127.0.0.1:$API_PORT/$API_NAME/delete/file -X POST -d "path=$deletefilepath"
+curl http://127.0.0.1:$API_PORT/$API_NAME/delete/file -X POST -d "path=$deletefilepath" -d "token=$AUTH_TOKEN"
 
 echo delete folder
-curl http://127.0.0.1:$API_PORT/$API_NAME/delete/folder -X POST -d "path=$deletefolderpath"
+curl http://127.0.0.1:$API_PORT/$API_NAME/delete/folder -X POST -d "path=$deletefolderpath" -d "token=$AUTH_TOKEN"
 
 
 
